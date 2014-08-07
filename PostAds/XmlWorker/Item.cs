c28e0b92ff6920c2eed28f7b6f5d869efa97fb3f
Yura Peleh <agronom.pele@gmail.com>
2014-08-07 23:11:40 +0300

    Create CnangeBaseXmlWorker class
    Create CnangeBaseXmlWorker class for inserting, editing, deleting
    existing data in xml database

--- PostAds/XmlWorker/Item.cs
+++ PostAds/XmlWorker/Item.cs
@@ -10,7 +10,7 @@ namespace Motorcycle.XmlWorker
             
         }
 
-        public Item(string id, int m, int p, int u)
+        public Item(string id, string m, string p, string u)
         {
             this.Id = id;
             this.M = m;
@@ -22,9 +22,9 @@ namespace Motorcycle.XmlWorker
         }
 
         public string Id { get;  set; }
-        public int M { get;  set; }
-        public int P { get;  set; }
-        public int U { get;  set; }
+        public string M { get;  set; }
+        public string P { get;  set; }
+        public string U { get;  set; }
 
         public List<Value> Values { get;  set; }
 
