a69bd9770297eb45891beba38f81e93a1d97907a
Yura Peleh <agronom.pele@gmail.com>
2014-09-01 23:11:49 +0300

    Create InfoHolder

---
+++ PostAds/Config/Data/DicHolder.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Motorcycle.Config.Data
+{
+    public class DicHolder
+    {
+        public Dictionary<string, string> DataDictionary { get; set; }
+
+        public Dictionary<string, string> FileDictionary { get; set; }
+    }
+}
---
+++ PostAds/Config/Data/InfoHolder.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Motorcycle.Config.Data
+{
+    public class InfoHolder
+    {
+        public SiteEnum Site { get; set; }
+
+        public ProductEnum Type { get; set; }
+
+        public List<DicHolder> Data { get; set; }
+    }
+}
