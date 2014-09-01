0c07a9fc43cd6d6e8583bd28c5364bea3e1e4bb0
mazanuj <mazanuj@gmail.com>
2014-09-01 18:08:04 +0300

    Bugs

--- PostAds/Config/Data/ReturnData.cs
+++ PostAds/Config/Data/ReturnData.cs
@@ -1,22 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Windows.Documents;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Motorcycle.Config.Data
 {
     internal static class ReturnData
     {
-        internal static List<Dictionary<string, string>> Moto(List<string> list, IList<byte> flag)
+        internal static List<Dictionary<string, string>>[] Moto(IEnumerable<string> list, IList<byte> flag)
         {
+            var returnArray = new List<Dictionary<string, string>>[3];
             if (flag[0] == 1)
             {
-                var motoData = new List<Dictionary<string, string>>();
-                foreach (var row in list)
-                {
-                    var t = MotosaleData.GetData(new List<string>(row.Split('\t')));
-                    motoData.Add(t);
-                }
+                var motoData = list.Select(row => MotosaleData.GetData(new List<string>(row.Split('\t')))).Select(t => (Dictionary<string, string>) t).ToList();
+                returnArray[0] = motoData;
             }
+            return returnArray;
         }
     }
 }
