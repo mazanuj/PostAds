c932279150e68ac3731ddc8f9b8a4c63d10aa57b
mazanuj <mazanuj@gmail.com>
2014-09-02 20:40:06 +0300

    Bug fixes in InfoHolder

--- PostAds/Config/Data/InfoHolder.cs
+++ PostAds/Config/Data/InfoHolder.cs
@@ -13,32 +13,6 @@ namespace Motorcycle.Config.Data
         public ProductEnum Type { get; set; }
 
         public List<DicHolder> Data { get; set; }
-
-        public InfoHolder()
-        {
-            Data = new List<DicHolder>();
-        }
-    }
-
-    public class DicHolder
-    {        
-        public Dictionary<string, string> DataDictionary { get; set; }
-
-        public Dictionary<string, string> FileDictionary { get; set; }
-    }
-
-    public enum ProductEnum
-    {
-        Motorcycle,
-        Spare,
-        Equip
-    }
-
-    public enum SiteEnum
-    {
-        MotoSale,
-        Proday2Kolesa,
-        UsedAuto
     }
 
     public class DicHolder
