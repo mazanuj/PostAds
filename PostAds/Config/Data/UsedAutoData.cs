5d8a328b4730d6ba608a82c4f9fa5aa44045cf74
mazanuj <mazanuj@gmail.com>
2014-09-02 15:31:17 +0300

    Change dictionaries

--- PostAds/Config/Data/UsedAutoData.cs
+++ PostAds/Config/Data/UsedAutoData.cs
@@ -53,36 +53,12 @@ namespace Motorcycle.Config.Data
             {
                 DataDictionary = new Dictionary<string, string>
                 {
-                    {"cid", "83"},
-                    {"act", "add"},
-                    {"rid", "3"},
-                    {"input[1]", "19"},
-                    {"input[153]", "model"},
-                    {"input[3]", "1"},
-                    {"price", "1000"},
-                    {"currency", "1"},
-                    {"input[4]", "5555"},
-                    {"input[200]", "600"},
-                    {"input[8]", "110"},
-                    {"input[18]", "1"},
-                    {"input[61]", "1"},
-                    {"input[6]", "1"},
-                    {"input[5]", "color"},
-                    {"input[194]", "1"},
-                    {"input[15]", "on"},
-                    {"input[151]", "on"},
-                    {"input[219]", "on"},
-                    {"description", "message"},
-                    {"photos", ""},
-                    {"main_photo", ""},
-                    {"user[0]", "name"},
-                    {"user[1]", "380676776767"},
-                    {"user[2]", ""},
-                    {"user[3]", ""},
-                    {"user[4]", ""},
-                    {"user[5]", "mail@ma.ma"}
+                    {"", ""}
                 },
-                FileDictionary = new Dictionary<string, string> {{"file", "virginia.jpg"}}
+                FileDictionary = new Dictionary<string, string>
+                {
+                    {"", ""}
+                }
             };
         }
 
@@ -94,36 +70,12 @@ namespace Motorcycle.Config.Data
             {
                 DataDictionary = new Dictionary<string, string>
                 {
-                    {"cid", "83"},
-                    {"act", "add"},
-                    {"rid", "3"},
-                    {"input[1]", "19"},
-                    {"input[153]", "model"},
-                    {"input[3]", "1"},
-                    {"price", "1000"},
-                    {"currency", "1"},
-                    {"input[4]", "5555"},
-                    {"input[200]", "600"},
-                    {"input[8]", "110"},
-                    {"input[18]", "1"},
-                    {"input[61]", "1"},
-                    {"input[6]", "1"},
-                    {"input[5]", "color"},
-                    {"input[194]", "1"},
-                    {"input[15]", "on"},
-                    {"input[151]", "on"},
-                    {"input[219]", "on"},
-                    {"description", "message"},
-                    {"photos", ""},
-                    {"main_photo", ""},
-                    {"user[0]", "name"},
-                    {"user[1]", "380676776767"},
-                    {"user[2]", ""},
-                    {"user[3]", ""},
-                    {"user[4]", ""},
-                    {"user[5]", "mail@ma.ma"}
+                    {"", ""}
                 },
-                FileDictionary = new Dictionary<string, string> {{"file", "virginia.jpg"}}
+                FileDictionary = new Dictionary<string, string>
+                {
+                    {"", ""}
+                }
             };
         }
     }
