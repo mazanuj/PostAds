e185a0bda6ff013c0e9215fe98797461ae63e7f8
mazanuj <mazanuj@gmail.com>
2014-09-16 02:04:03 +0300

    Create 'Factories' folder

---
+++ PostAds/Factories/PostOnSiteFactory.cs
@@ -0,0 +1,27 @@
+﻿using Motorcycle.Config.Data;
+using Motorcycle.Interfaces;
+using Motorcycle.Sites;
+
+namespace Motorcycle.Factories
+{
+    internal static class PostOnSiteFactory
+    {
+        public static IPostOnSite GetPostOnSite(SiteEnum site)
+        {
+            switch (site)
+            {
+                case SiteEnum.Proday2Kolesa:
+                    return new Proday2Kolesa();
+
+                case SiteEnum.UsedAuto:
+                    return new UsedAuto();
+
+                case SiteEnum.MotoSale:
+                    return new MotoSale();
+
+                default:
+                    return new Proday2Kolesa();
+            }
+        }
+    }
+}
---
+++ PostAds/Factories/SiteDataFactory.cs
@@ -0,0 +1,26 @@
+﻿using Motorcycle.Config.Data;
+using Motorcycle.Interfaces;
+
+namespace Motorcycle.Factories
+{
+    internal static class SiteDataFactory
+    {
+        public static ISiteData GetSiteData(SiteEnum site)
+        {
+            switch (site)
+            {
+                case SiteEnum.Proday2Kolesa:
+                    return new DvaKolesaData();
+
+                case SiteEnum.UsedAuto:
+                    return new UsedAutoData();
+
+                case SiteEnum.MotoSale:
+                    return new MotosaleData();
+
+                default:
+                    return new DvaKolesaData();
+            }
+        }
+    }
+}
