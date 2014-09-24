5d7b890d0e5663732e4e341c228c9968d0446e37
mazanuj <mazanuj@gmail.com>
2014-09-24 23:43:13 +0300

    Add RemoveEntries class

---
+++ PostAds/Config/Data/RemoveEntries.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Motorcycle.XmlWorker;
+
+namespace Motorcycle.Config.Data
+{
+    internal class RemoveEntries
+    {
+        public static bool Remove(DicHolder dicHol, ProductEnum product)
+        {
+            var locker = new Object();
+            var direction = string.Empty;
+
+            switch (product)
+            {
+                case ProductEnum.Motorcycle:
+                    direction = "moto";
+                    break;
+                case ProductEnum.Spare:
+                    direction = "spare";
+                    break;
+                case ProductEnum.Equip:
+                    direction = "equip";
+                    break;
+            }
+            if (direction == string.Empty) return false;
+
+            lock (locker)
+            {
+                var rows = File.ReadAllLines(FilePathXmlWorker.GetFilePath(direction)).ToList();
+                rows[dicHol.LineNum] = string.Empty;
+                File.WriteAllLines(FilePathXmlWorker.GetFilePath(direction), rows);
+            }
+
+            return true;
+        }
+    }
+}
